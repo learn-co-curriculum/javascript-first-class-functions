c7f85a167b76734cb646199d8d19a6a3a8d2e077
Thomas Tuts <thomas@thomastuts.com>
2016-07-01 11:12:52 +0200

    Add all code to solution file

---
+++ index.js
@@ -0,0 +1,68 @@
+function runFiveMiles() {
+  console.log('Go for a five-mile run');
+}
+
+function liftWeights() {
+  console.log('Pump iron');
+}
+
+function swimFortyLaps() {
+  console.log('Swim 40 laps');
+}
+
+function exerciseRoutine(postRunActivity) {
+  runFiveMiles();
+  postRunActivity();
+}
+
+function Monday() {
+  exerciseRoutine(liftWeights);
+}
+
+// Pilates time!
+function Tuesday() {
+  exerciseRoutine(function() {
+    console.log('Stretch! Work that core!');
+  });
+
+  // OR:
+  exerciseRoutine(() => {
+    console.log('Stretch! Work that core!');
+  });
+
+  // OR:
+  exerciseRoutine(() => console.log('Stretch! Work that core!'));
+}
+
+function Wednesday() {
+  exerciseRoutine(runFiveMiles);
+}
+
+function Thursday() {
+  exerciseRoutine(liftWeights);
+}
+
+function Friday() {
+  exerciseRoutine(swimFortyLaps);
+}
+
+function morningRoutine(exercise) {
+  var breakfast = null;
+
+  if (exercise === liftWeights) {
+    breakfast = 'protein bar';
+  } else if (exercise === swimFortyLaps) {
+    breakfast = 'kale smoothie';
+  } else {
+    breakfast = 'granola';
+  }
+
+  exerciseRoutine(exercise);
+
+  return function() {
+    console.log(`Nom nom nom, this ${breakfast} is delicious!`);
+  }
+}
+
+var afterExercise = morningRoutine(liftWeights);
+afterExercise();
